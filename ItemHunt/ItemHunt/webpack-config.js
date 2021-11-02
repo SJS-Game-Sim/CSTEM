5eed837ee95b20009779d4c0507c66f2a2180a5e
Todd McCracken <mccracken.t087171@stu.sanjac.edu>
2021-11-02 00:42:28 -0500

    Hopefully got nodejs setup correctly

---
+++ ItemHunt/ItemHunt/webpack-config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    devtool: 'source-map',
+    entry: "./app.tsx",
+    mode: "development",
+    output: {
+        filename: "./app-bundle.js",
+        devtoolModuleFilenameTemplate: '[resource-path]'  // removes the webpack:/// prefix
+    },
+    resolve: {
+        extensions: ['.Webpack.js', '.web.js', '.ts', '.js', '.jsx', '.tsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'ts-loader'
+                }
+            }
+        ]
+    }
+}
