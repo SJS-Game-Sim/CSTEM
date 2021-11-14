d47f8077dab7f1abd820e1f803a5ab880b5fe54c
Kevin <Segarra.K951715@stu.sanjac.edu>
2021-11-14 17:44:36 -0600

    app/itemHunt.js add drag and drop functionality

--- app/itemHunt.js
+++ app/itemHunt.js
@@ -20,3 +20,40 @@ newImg.setAttribute("alt", alt);
 newImg.setAttribute("onclick", onclick);
 
 addHere.appendChild(newImg);
+
+newImg.onmousedown = function(event) {
+  // (1) prepare to moving: make absolute and on top by z-index
+  newImg.style.position = 'absolute';
+  newImg.style.zIndex = 1000;
+
+  // move it out of any current parents directly into body
+  // to make it positioned relative to the body
+  document.body.append(newImg);
+
+  // centers the ball at (pageX, pageY) coordinates
+  function moveAt(pageX, pageY) {
+    newImg.style.left = pageX - newImg.offsetWidth / 2 + 'px';
+    newImg.style.top = pageY - newImg.offsetHeight / 2 + 'px';
+  }
+
+  // move our absolutely positioned ball under the pointer
+  moveAt(event.pageX, event.pageY);
+
+  function onMouseMove(event) {
+    moveAt(event.pageX, event.pageY);
+  }
+
+  // (2) move the ball on mousemove
+  document.addEventListener('mousemove', onMouseMove);
+
+  // (3) drop the ball, remove unneeded handlers
+  newImg.onmouseup = function() {
+    document.removeEventListener('mousemove', onMouseMove);
+    newImg.onmouseup = null;
+  };
+
+  newImg.ondragstart = function() {
+    return false;
+  };
+
+};
