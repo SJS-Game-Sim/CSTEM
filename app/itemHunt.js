19f85333cba3e42086c25878fe5e25ce48c1eace
Kevin <Segarra.K951715@stu.sanjac.edu>
2021-11-20 01:05:17 -0600

    Add a drag and drop system

--- app/itemHunt.js
+++ app/itemHunt.js
@@ -57,3 +57,143 @@ newImg.onmousedown = function(event) {
   };
 
 };
+
+var body = "" +
+"<div id=\"container\">" +
+"      <div class=\"list\">" +
+"        <h1 id=\"name1\ class=\"cName\>TODO</h1>" +
+"        <div class=\"draggingContainer\">" +
+"          <div class=\"card\" draggable=\"true\">" +
+"            <div class=\"title\ id=\"iTitle1\">Complete Header Design</div>" +
+"            <div class=\"description\" id=\"iDesc1\">" +
+"              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus" +
+"              sit amet volutpat dui. Cras sed vulputate augue, vel egestas quam." +
+"              Donec condimentum vitae orci sed bibendum." +
+"            </div>" +
+"          </div>" +
+"" +
+"          <div class=\"card\" draggable=\"true\">" +
+"            <div class=\"title\" id=\"iTitle2\">Task number 2</div>" +
+"            <div class=\"description\" id=\"iDesc2\">" +
+"              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus" +
+"              sit amet volutpat dui. Cras sed vulputate augue, vel egestas quam." +
+"              Donec condimentum vitae orci sed bibendum." +
+"            </div>" +
+"          </div>" +
+"" +
+"          <div class=\"card\" draggable=\"true\">" +
+"            <div class=\"title\" id=\"iTitle3\">Task number 3</div>" +
+"            <div class=\"description\" id=\"iDesc3\">" +
+"              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus" +
+"              sit amet volutpat dui. Cras sed vulputate augue, vel egestas quam." +
+"              Donec condimentum vitae orci sed bibendum." +
+"            </div>" +
+"          </div>" +
+"        </div>" +
+"      </div>" +
+"" +
+"      <div class=\"list\">" +
+"        <h1 id=\"name2\ class=\"cName\>In Progress</h1>" +
+"        <div class=\"draggingContainer\">" +
+"          <div class=\"card\" draggable=\"true\">" +
+"            <div class=\"title\"id=\"iTitle4\">Task number 5</div>" +
+"            <div class=\"description\" id=\"iDesc4\">" +
+"              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus" +
+"              sit amet volutpat dui. Cras sed vulputate augue, vel egestas quam." +
+"              Donec condimentum vitae orci sed bibendum." +
+"            </div>" +
+"          </div>" +
+"          <div class=\"card\" draggable=\"true\">" +
+"            <div class=\"title\" id=\"iTitle5\">Task number 6</div>" +
+"            <div class=\"description\" id=\"iDesc5\">" +
+"              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus" +
+"              sit amet volutpat dui. Cras sed vulputate augue, vel egestas quam." +
+"              Donec condimentum vitae orci sed bibendum." +
+"            </div>" +
+"          </div>" +
+"        </div>" +
+"      </div>" +
+"" +
+"      <div class=\"list\">" +
+"        <h1 id=\"name3\ class=\"cName\>In Review</h1>" +
+"        <div class=\"draggingContainer\">" +
+"          <div class=\"card\" draggable=\"true\">" +
+"            <div class=\"title\" id=\"iTitle6\">Task number 7</div>" +
+"            <div class=\"description\" id=\"iDesc6\">" +
+"              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus" +
+"              sit amet volutpat dui. Cras sed vulputate augue, vel egestas quam." +
+"              Donec condimentum vitae orci sed bibendum." +
+"            </div>" +
+"          </div>" +
+"        </div>" +
+"      </div>" +
+"" +
+"      <div class=\"list\">" +
+"        <h1 id=\"name4\ class=\"cName\>Done</h1>" +
+"        <div class=\"draggingContainer\">" +
+"          <div class=\"card\" draggable=\"true\">" +
+"            <div class=\"title\" id=\"iTitle7\">Task number 8</div>" +
+"            <div class=\"description\" id=\"iDesc7\">" +
+"              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus" +
+"              sit amet volutpat dui. Cras sed vulputate augue, vel egestas quam." +
+"              Donec condimentum vitae orci sed bibendum." +
+"            </div>" +
+"          </div>" +
+"        </div>" +
+"      </div>" +
+"    </div>" +
+"";
+
+document.querySelector('body').innerHTML += (body);
+
+let cards = document.querySelectorAll('.card');
+let lists = document.querySelectorAll('.list');
+
+
+cards.forEach((card)=>{
+    registerEventsOnCard(card);
+});
+
+lists.forEach((list)=>{
+    list.addEventListener('dragover', (e)=>{
+        e.preventDefault();
+        let draggingCard = document.querySelector('.dragging');
+        let cardAfterDraggingCard = getCardAfterDraggingCard(list, e.clientY);
+        if(cardAfterDraggingCard){
+
+                cardAfterDraggingCard.parentNode.insertBefore(draggingCard, cardAfterDraggingCard);
+        } else{
+            list.appendChild(draggingCard);
+        }
+
+    });
+});
+
+function getCardAfterDraggingCard(list, yDraggingCard){
+
+    let listCards = [...list.querySelectorAll('.card:not(.dragging)')];
+
+    return listCards.reduce((closestCard, nextCard)=>{
+        let nextCardRect = nextCard.getBoundingClientRect();
+        let offset = yDraggingCard - nextCardRect.top - nextCardRect.height /2;
+
+        if(offset < 0 && offset > closestCard.offset){
+            return {offset, element: nextCard}
+        } else{
+            return closestCard;
+        }
+
+    }, {offset: Number.NEGATIVE_INFINITY}).element;
+
+}
+
+function registerEventsOnCard(card){
+    card.addEventListener('dragstart', (e)=>{
+        card.classList.add('dragging');
+    });
+
+
+    card.addEventListener('dragend', (e)=>{
+        card.classList.remove('dragging');
+    });
+}
