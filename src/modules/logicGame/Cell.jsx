10dc25f9f261a093f656c33255c0e22c2e0bf447
James Isaacks <James.Isaacks@sjcd.edu>
2022-03-22 21:47:09 -0500

    Basic drop area for bubbles. May satisfy #49

--- src/modules/logicGame/Cell.jsx
+++ src/modules/logicGame/Cell.jsx
@@ -1,6 +1,21 @@
+import React from 'react'
 import { useDrop } from 'react-dnd'
 import { ItemTypes } from './ItemTypes'
+import './logicGame.css'
 
-function Cell({ children }) {
-    const 
+function Cell() {
+    const [{ canDrop, isOver }, drop] = useDrop(() => ({
+        // Type to accept. Defined in ItemTypes.
+        accept: 'Bubble',
+        //Collection
+        collect: (monitor) => ({
+            isOver: monitor.isOver(),
+            canDrop: monitor.canDrop()
+        })
+    }))
+
+    return (
+        <div className='cell' ref={drop}>
+        </div>
+    )
 }
