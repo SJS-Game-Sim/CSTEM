3249c249464873f488addf1b89b3e88d8444e79b
James Isaacks <James.Isaacks@sjcd.edu>
2022-03-25 19:32:15 -0500

    Added drag/drop to Bubble. Bubble should now handle drag/drop interactions within its container. See react-dnd simple sortable example: https://react-dnd.github.io/react-dnd/examples/sortable/simple

--- src/modules/logicGame/Bubble.jsx
+++ src/modules/logicGame/Bubble.jsx
@@ -1,24 +1,50 @@
-import React from 'react'
-import { useDrag } from 'react-dnd'
+import React, { useRef } from 'react'
+import { useDrag, useDrop } from 'react-dnd'
 import { ItemTypes } from './itemTypes'
 import './LogicGame.css'
 
-const Bubble = props => {
-    const id = props
-    const [{ isDragging }, drag] = useDrag(() => ({
+export const Bubble = ({id, text, index, moveBubble}) => {
+    const ref = useRef(null)
+
+    const [{ handlerId }, drop] = useDrop({
+        accept: ItemTypes.BUBBLE,
+        collect(monitor) {
+            return {
+                handlerId: monitor.getHandlerId(),
+            }
+        },
+        hover(item, monitor) {
+            if (!ref.current) {
+                return
+            }
+            const dragIndex = item.index
+            const hoverIndex = props.index
+            if (dragIndex === hoverIndex) {
+                return
+            }
+
+            moveBubble(dragIndex, hoverIndex)
+            item.index = hoverIndex
+        }
+    })
+
+    const [{ isDragging }, drag] = useDrag({
         // Type works with drop locations to determine if this item can be dropped there.
         type: ItemTypes.BUBBLE,
+        item: () => {
+            return { id, index }
+        },
         // Collect is a builtin property of react-dnd drag hook. Pulls state from DnD
         collect: (monitor) => ({
             isDragging: monitor.isDragging()
         })
-    }))
+    })
+
+    drag(drop(ref))
 
     return (
-        <div className="bubble" ref={drag}>
-            <h3>{props.text}</h3>
+        <div className="bubble" ref={ref} data-handler-id={handlerId}>
+            <h3>{text}</h3>
         </div>
     )
 }
-
-export default Bubble
